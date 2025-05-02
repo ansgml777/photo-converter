bd20c47ca654a4067d20e455bf73a842165f1815
ansgml777 <dingdingding0428@gmail.com>
2025-05-02 12:53:27 +0900

    Update u2net_server.py

--- u2net_server.py
+++ u2net_server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, send_file
+from flask_cors import CORS
 from rembg import remove
 from PIL import Image
 import io
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/remove_bg', methods=['POST'])
 def remove_bg():
